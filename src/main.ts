fdbed76deca6e6221e01bdf7821c033011e1c792
Twiliness <729294652hs@gmail.com>
2022-03-02 13:52:10 +0800

    fix mistakes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 
 // @ts-ignore
-import MathJax from "../dist/vue3-mathjax3.es.js";
+import MathJax from "../dist/vue3-mathjax.es.js";
 import App from "./App.vue";
 
 createApp(App).use(MathJax).mount("#app");
